c384fd894d73f699651173c75978b4daea11ed63
Augusto Tomasi <augustopreisthomasi@gmail.com>
2025-04-01 16:41:49 -0300

    adiciona padding para evitar sobrepor a camera frontal do celular

--- app/src/main/java/com/example/sirius/MainActivity.kt
+++ app/src/main/java/com/example/sirius/MainActivity.kt
@@ -3,11 +3,28 @@ package com.example.sirius
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import com.example.sirius.ui.theme.SiriusTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+
+    // Avoid using the front camera part
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+    ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { view, insets ->
+      val displayCutout = insets.displayCutout
+
+      if (displayCutout != null) {
+        val safeInsetTop = displayCutout.safeInsetTop
+        val safeInsetBottom = displayCutout.safeInsetBottom
+
+        view.setPadding(0, safeInsetTop, 0, safeInsetBottom)
+      }
+
+      insets
+    }
 
     setContent {
       SiriusTheme {
